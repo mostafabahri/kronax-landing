27fa32d778058c8980f3342d0554ad15bcd842e9
Mostafa Bahri <mostafabahri.dev@gmail.com>
2020-04-10 10:23:15 +0430

    Add opacity transition

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.querySelector("#mobile-nav-btn").addEventListener("click", () => {
   // toggle menu div
-  document.querySelector("#mobile-nav").classList.toggle("hidden");
+  document.querySelector("#mobile-nav").classList.toggle("closed");
   // toggle menu icon
   document.querySelector(['[data-js="open-icon"]']).classList.toggle("hidden");
   document.querySelector(['[data-js="close-icon"]']).classList.toggle("hidden");
